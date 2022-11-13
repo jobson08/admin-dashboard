c252ebcec4f75f20e00ebd884f8805b52153ef48
jobson <jammeloalves@gmail.com>
2022-11-13 09:28:39 -0300

    Dashboard finalizado respomsivel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
@@ -26,66 +27,59 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) =>(
 
 const Navbar = () => {
   const { currentColor, activeMenu, setActiveMenu, handleClick, isClicked, setScreenSize, screenSize } = useStateContext();
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
 
-useEffect(() => {
-  const handleRsize = () => setScreenSize
-  (window.innerWidth);
+    window.addEventListener('resize', handleResize);
 
-  window.addEventListener('resize', handleRsize);
+    handleResize();
 
-  handleRsize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-  return () => window.removeEventListener ('resize', handleRsize);
-}, [setScreenSize]);
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
-useEffect(() =>{
-  if(screenSize <= 900) {
-    setActiveMenu(false);
-  } else {
-    setActiveMenu(true);
-  }
-},[screenSize, setActiveMenu]);
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
-    <div className='flex justify-between p-2 md:mx-6 relative'>
+   <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
 
-      <NavButton title="Menu"
-       customFunc={() => setActiveMenu ((prevAtiveMenu) =>
-        !prevAtiveMenu)} color={currentColor}  icon={<AiOutlineMenu/>}/>
-
-        <div className='flex'>
-        <NavButton title="Cart"
-        customFunc={() => handleClick ('cart')}
-        color={currentColor}  icon={<FiShoppingCart />} />
-
-      <NavButton title="Chat" dotColor='#03C9D7'
-       customFunc={() => handleClick ('chat')}
-        color={currentColor}  icon={<BsChatLeft/>} />
-
-      <NavButton title="Notifications" dotColor='#03C9D7'
-       customFunc={() => handleClick ('notification') }
-        color={currentColor}  icon={<RiNotification3Line/>} />
-
-        <TooltipComponent content='Profile' position ='BottomCenter'>
-          <div   className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
-            onClick={() => handleClick('userProfile')}>
-            <img className='rounded-full w-8 h-8' src={avatar} />
+      <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
+      <div className="flex">
+        <NavButton title="Cart" customFunc={() => handleClick('cart')} color={currentColor} icon={<FiShoppingCart />} />
+        <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick('chat')} color={currentColor} icon={<BsChatLeft />} />
+        <NavButton title="Notification" dotColor="rgb(254, 201, 15)" customFunc={() => handleClick('notification')} color={currentColor} icon={<RiNotification3Line />} />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')}
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt="user-profile"
+            />
             <p>
-              <span className='text-gray-400 text-14'>Olá</span>
-              {' '}
-              <span className='text-gray-400 font-bold ml-1 text-14'>Job</span>
+              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
             </p>
-
-            <MdKeyboardArrowDown  className='text-gray-400 text-14'/>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
         </TooltipComponent>
 
-        {isClicked.cart && <Cart />}
-        {isClicked.chat && <Chat />}
-        {isClicked.notification && <Notification />}
-        {isClicked.userProfile && <UserProfile />}
-        </div>
-
+        {isClicked.cart && (<Cart />)}
+        {isClicked.chat && (<Chat />)}
+        {isClicked.notification && (<Notification />)}
+        {isClicked.userProfile && (<UserProfile />)}
+      </div>
     </div>
   )
 }
